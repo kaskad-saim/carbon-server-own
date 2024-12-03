ed7e4fe633b2276175b6911fdc7b12fd996f966e
Dmitry <melkov.dmitriy40@gmail.com>
2024-12-03 16:20:28 +0500

    add simulator uzliUcheta

--- src/services/modbusSimulator.js
+++ src/services/modbusSimulator.js
@@ -3,6 +3,26 @@ export class ModbusSimulator {
     this.port = port;
     this.isConnected = true;
     this.currentValues = {}; // Хранит текущие значения для каждого параметра
+
+    // Карта устройств для быстрого доступа по deviceLabel
+    this.devices = {
+      // Существующие устройства
+      pechiVr: this.pechiVr,
+      sushilki: this.sushilki,
+      mpa: this.mpa,
+      mills: this.mills,
+      mill10b: this.mill10b,
+      reactor296: this.reactor296,
+
+      // Новые устройства
+      DE093: this.DE093,
+      DD972: this.DD972,
+      DD973: this.DD973,
+      DD576: this.DD576,
+      DD569: this.DD569,
+      DD923: this.DD923,
+      DD924: this.DD924,
+    };
   }
 
   async connect() {
@@ -10,7 +30,7 @@ export class ModbusSimulator {
     return Promise.resolve();
   }
 
-  // Объекты для каждого типа оборудования
+  // Существующие объекты для каждого типа оборудования
   pechiVr = {
     temperatureAddressesList: [
       0x0000, 0x0002, 0x0004, 0x0006, 0x0008, 0x0012, 0x000A, 0x000C, 0x004E, 0x000E, 0x0010, 0x004C, 0x0014, 0x0016
@@ -119,17 +139,227 @@ export class ModbusSimulator {
     }
   };
 
+  // Новые объекты для дополнительных устройств
+
+  // DE093
+  DE093 = {
+    addressesList: {
+      'Гкал/ч DE093': 0x0004,
+      'Температура DE093': 0x0006,
+      'Давление DE093': 0x0008,
+      'Куб/ч DE093': 0x000a,
+      'Тонн/ч DE093': 0x000c,
+      'Накопленно тонн DE093': 0x000e,
+    },
+    getRange(address) {
+      switch (address) {
+        case 0x0004: // Гкал/ч
+          return { min: 0, max: 10, step: 0.1 };
+        case 0x0006: // Температура
+          return { min: 0, max: 200, step: 1 };
+        case 0x0008: // Давление
+          return { min: 0, max: 5, step: 0.1 };
+        case 0x000a: // Куб/ч
+          return { min: 0, max: 1000, step: 10 };
+        case 0x000c: // Тонн/ч
+          return { min: 0, max: 2, step: 0.1 };
+        case 0x000e: // Накопленно тонн
+          return { min: 0, max: 100, step: 1 };
+        default:
+          return { min: 0, max: 100, step: 10 }; // Значения по умолчанию
+      }
+    }
+  };
+
+  // DD972
+  DD972 = {
+    addressesList: {
+      'Гкал/ч DD972': 0x0004,
+      'Температура DD972': 0x0006,
+      'Давление DD972': 0x0008,
+      'Куб/ч DD972': 0x000a,
+      'Тонн/ч DD972': 0x000c,
+      'Накопленно тонн DD972': 0x000e,
+    },
+    getRange(address) {
+      switch (address) {
+        case 0x0004: // Гкал/ч
+          return { min: 0, max: 10, step: 0.1 };
+        case 0x0006: // Температура
+          return { min: 0, max: 200, step: 1 };
+        case 0x0008: // Давление
+          return { min: 0, max: 5, step: 0.1 };
+        case 0x000a: // Куб/ч
+          return { min: 0, max: 1000, step: 10 };
+        case 0x000c: // Тонн/ч
+          return { min: 0, max: 2, step: 0.1 };
+        case 0x000e: // Накопленно тонн
+          return { min: 0, max: 100, step: 1 };
+        default:
+          return { min: 0, max: 100, step: 10 }; // Значения по умолчанию
+      }
+    }
+  };
+
+  // DD973
+  DD973 = {
+    addressesList: {
+      'Гкал/ч DD973': 0x0004,
+      'Температура DD973': 0x0006,
+      'Давление DD973': 0x0008,
+      'Куб/ч DD973': 0x000a,
+      'Тонн/ч DD973': 0x000c,
+      'Накопленно тонн DD973': 0x000e,
+    },
+    getRange(address) {
+      switch (address) {
+        case 0x0004: // Гкал/ч
+          return { min: 0, max: 10, step: 0.1 };
+        case 0x0006: // Температура
+          return { min: 0, max: 200, step: 1 };
+        case 0x0008: // Давление
+          return { min: 0, max: 5, step: 0.1 };
+        case 0x000a: // Куб/ч
+          return { min: 0, max: 1000, step: 10 };
+        case 0x000c: // Тонн/ч
+          return { min: 0, max: 2, step: 0.1 };
+        case 0x000e: // Накопленно тонн
+          return { min: 0, max: 100, step: 1 };
+        default:
+          return { min: 0, max: 100, step: 10 }; // Значения по умолчанию
+      }
+    }
+  };
+
+  // DD576
+  DD576 = {
+    addressesList: {
+      'Гкал/ч DD576': 0x0004,
+      'Температура DD576': 0x0006,
+      'Давление DD576': 0x0008,
+      'Куб/ч DD576': 0x000a,
+      'Тонн/ч DD576': 0x000c,
+      'Накопленно тонн DD576': 0x000e,
+    },
+    getRange(address) {
+      switch (address) {
+        case 0x0004: // Гкал/ч
+          return { min: 0, max: 10, step: 0.1 };
+        case 0x0006: // Температура
+          return { min: 0, max: 200, step: 1 };
+        case 0x0008: // Давление
+          return { min: 0, max: 5, step: 0.1 };
+        case 0x000a: // Куб/ч
+          return { min: 0, max: 1000, step: 10 };
+        case 0x000c: // Тонн/ч
+          return { min: 0, max: 2, step: 0.1 };
+        case 0x000e: // Накопленно тонн
+          return { min: 0, max: 100, step: 1 };
+        default:
+          return { min: 0, max: 100, step: 10 }; // Значения по умолчанию
+      }
+    }
+  };
+
+  // DD569
+  DD569 = {
+    addressesList: {
+      'Гкал/ч DD569': 0x0004,
+      'Температура DD569': 0x0006,
+      'Давление DD569': 0x0008,
+      'Куб/ч DD569': 0x000a,
+      'Тонн/ч DD569': 0x000c,
+      'Накопленно тонн DD569': 0x000e,
+    },
+    getRange(address) {
+      switch (address) {
+        case 0x0004: // Гкал/ч
+          return { min: 0, max: 10, step: 0.1 };
+        case 0x0006: // Температура
+          return { min: 0, max: 200, step: 1 };
+        case 0x0008: // Давление
+          return { min: 0, max: 5, step: 0.1 };
+        case 0x000a: // Куб/ч
+          return { min: 0, max: 1000, step: 10 };
+        case 0x000c: // Тонн/ч
+          return { min: 0, max: 2, step: 0.1 };
+        case 0x000e: // Накопленно тонн
+          return { min: 0, max: 100, step: 1 };
+        default:
+          return { min: 0, max: 100, step: 10 }; // Значения по умолчанию
+      }
+    }
+  };
+
+  // DD923
+  DD923 = {
+    addressesList: {
+      'Гкал/ч DD923': 0x0004,
+      'Температура DD923': 0x0006,
+      'Давление DD923': 0x0008,
+      'Куб/ч DD923': 0x000a,
+      'Тонн/ч DD923': 0x000c,
+      'Накопленно тонн DD923': 0x000e,
+    },
+    getRange(address) {
+      switch (address) {
+        case 0x0004: // Гкал/ч
+          return { min: 0, max: 10, step: 0.1 };
+        case 0x0006: // Температура
+          return { min: 0, max: 200, step: 1 };
+        case 0x0008: // Давление
+          return { min: 0, max: 5, step: 0.1 };
+        case 0x000a: // Куб/ч
+          return { min: 0, max: 1000, step: 10 };
+        case 0x000c: // Тонн/ч
+          return { min: 0, max: 2, step: 0.1 };
+        case 0x000e: // Накопленно тонн
+          return { min: 0, max: 100, step: 1 };
+        default:
+          return { min: 0, max: 100, step: 10 }; // Значения по умолчанию
+      }
+    }
+  };
+
+  // DD924
+  DD924 = {
+    addressesList: {
+      'Гкал/ч DD924': 0x0004,
+      'Температура DD924': 0x0006,
+      'Давление DD924': 0x0008,
+      'Куб/ч DD924': 0x000a,
+      'Тонн/ч DD924': 0x000c,
+      'Накопленно тонн DD924': 0x000e,
+    },
+    getRange(address) {
+      switch (address) {
+        case 0x0004: // Гкал/ч
+          return { min: 0, max: 10, step: 0.1 };
+        case 0x0006: // Температура
+          return { min: 0, max: 200, step: 1 };
+        case 0x0008: // Давление
+          return { min: 0, max: 5, step: 0.1 };
+        case 0x000a: // Куб/ч
+          return { min: 0, max: 1000, step: 10 };
+        case 0x000c: // Тонн/ч
+          return { min: 0, max: 2, step: 0.1 };
+        case 0x000e: // Накопленно тонн
+          return { min: 0, max: 100, step: 1 };
+        default:
+          return { min: 0, max: 100, step: 10 }; // Значения по умолчанию
+      }
+    }
+  };
+
   // Метод для чтения значений с учётом объекта и диапазона
   async readFloat(deviceID, address, deviceLabel = '') {
     let range = null;
 
     // Проверка диапазонов для всех объектов
-    if (this.pechiVr.getRange(address, deviceLabel)) range = this.pechiVr.getRange(address, deviceLabel);
-    if (this.sushilki.getRange(address, deviceLabel)) range = this.sushilki.getRange(address, deviceLabel);
-    if (this.mpa.getRange(address)) range = this.mpa.getRange(address);
-    if (this.mills.getRange(address)) range = this.mills.getRange(address);
-    if (this.mill10b.getRange(address)) range = this.mill10b.getRange(address);
-    if (this.reactor296.getRange(address)) range = this.reactor296.getRange(address);
+    const device = this.devices[deviceLabel];
+    if (device && device.getRange) {
+      range = device.getRange(address, deviceLabel);
+    }
 
     // Если диапазон не найден, устанавливаем дефолтное значение
     if (!range) range = { min: 0, max: 100, step: 10 };
@@ -150,7 +380,7 @@ export class ModbusSimulator {
     if (newValue > range.max) newValue = range.max;
     if (newValue < range.min) newValue = range.min;
 
-    // Логирование изменений
+    // Логирование изменений (можно раскомментировать при необходимости)
     // console.log(`Старое значение: ${this.currentValues[key]}, Изменение: ${change}, Новое значение: ${newValue}`);
 
     this.currentValues[key] = newValue;
@@ -158,16 +388,14 @@ export class ModbusSimulator {
   }
 
   // Метод для чтения int16 значений
-  async readInt16(deviceID, address) {
+  async readInt16(deviceID, address, deviceLabel = '') {
     let range = null;
 
     // Проверка диапазонов для всех объектов
-    if (this.pechiVr.getRange(address)) range = this.pechiVr.getRange(address);
-    if (this.sushilki.getRange(address)) range = this.sushilki.getRange(address);
-    if (this.mpa.getRange(address)) range = this.mpa.getRange(address);
-    if (this.mills.getRange(address)) range = this.mills.getRange(address);
-    if (this.mill10b.getRange(address)) range = this.mill10b.getRange(address);
-    if (this.reactor296.getRange(address)) range = this.reactor296.getRange(address);
+    const device = this.devices[deviceLabel];
+    if (device && device.getRange) {
+      range = device.getRange(address);
+    }
 
     if (!range) range = { min: 0, max: 100, step: 10 };  // Дефолтные значения
 
@@ -188,6 +416,28 @@ export class ModbusSimulator {
 
     this.currentValues[key] = newValue;
     return parseInt(newValue.toFixed(0));  // Возвращаем целочисленное значение
+  }
+
+  // Метод для чтения float LE (Little Endian)
+  async readFloatLE(deviceID, address, deviceLabel = '') {
+    // Симуляция чтения float LE аналогично readFloat
+    // В реальном случае может потребоваться другая логика
+    return await this.readFloat(deviceID, address, deviceLabel);
+  }
+
+  // Метод для чтения float BE (Big Endian)
+  async readFloatBE(deviceID, address, deviceLabel = '') {
+    // Симуляция чтения float BE аналогично readFloat
+    // В реальном случае может потребоваться другая логика
+    return await this.readFloat(deviceID, address, deviceLabel);
+  }
+
+  // Метод для чтения int32 значений
+  async readInt32(deviceID, address, deviceLabel = '') {
+    // Для симуляции int32 можно использовать два int16 и объединить их
+    const high = await this.readInt16(deviceID, address, deviceLabel);
+    const low = await this.readInt16(deviceID, address + 1, deviceLabel);
+    return high * 65536 + low;
   }
 
   // Метод для инициализации значений
